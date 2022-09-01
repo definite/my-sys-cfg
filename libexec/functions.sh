9cfff025f25a7a921c335a2ffe996e5b7a1e8765
Ding-Yi Chen <dchen@redhat.com>
2022-09-01 17:03:17 +1000

    fix(function): fix msc_extract_section, use pure shell instead of awk refactor(function): use 2 leading space

--- libexec/functions.sh
+++ libexec/functions.sh
@@ -11,37 +11,37 @@ source $MSC_LIBEXEC_DIR/const.sh
 
 ###
 ### MSG_LOG_OPTIONS
-###     Options for logger. Default is '-s', i.e. also print error message to
-###     stderr.
-###     Default: -s
+###   Options for logger. Default is '-s', i.e. also print error message to
+###   stderr.
+###   Default: -s
 : ${MSC_LOG_OPTIONS:="-s"}
 ###
 ### MSC_LOG_TAG
-###     Marked the log with a specified tag.
-###     Default: my-sys-cfg
+###   Marked the log with a specified tag.
+###   Default: my-sys-cfg
 : ${MSC_LOG_TAG:=my-sys-cfg}
 
 ##
 ## MscLogWorstStatusCode
-##     Worst (Biggest) status code encountered
-##     This is mainly for the log level 'error', which means the eventually
-##     the program will fail, but no need to exit immediately.
+##   Worst (Biggest) status code encountered
+##   This is mainly for the log level 'error', which means the eventually
+##   the program will fail, but no need to exit immediately.
 MscLogWorstStatusCode=$MSC_EXIT_OK
 MscExitMsg=
 
 ###
 ### msc_array_get_value <array> <index>
-###     Return value of an array
-###     This function address the difference between
-###     how bash and zsh handles associative array
+###   Return value of an array
+###   This function address the difference between
+###   how bash and zsh handles associative array
 msc_array_get_value() {
-    if [ -n "${ZSH_NAME-}" ]; then
-        ## Use Parameter Expension P flag from ZSH
-        echo ${${(P)1}[$2]}
-    else
-        ## Need quote for associative array in bash
-        eval 'echo ${'$1"['"$2"']}"
-    fi
+  if [ -n "${ZSH_NAME-}" ]; then
+    ## Use Parameter Expension P flag from ZSH
+    echo ${${(P)1}[$2]}
+  else
+    ## Need quote for associative array in bash
+    eval 'echo ${'$1"['"$2"']}"
+  fi
 }
 
 ###
@@ -55,68 +55,68 @@ msc_array_get_value() {
 ###   Environments:
 ###     MSG_LOG_TAG: Tag the log message
 msc_log() {
-    local msg="$1"
+  local msg="$1"
+  shift
+  local logLevel
+  local finalMsg="$MSC_LOG_PREFIX"
+
+  ## Detemine log level
+  if [ -n "${1:-}" ]; then
+    logLevel=$1
     shift
-    local logLevel
-    local finalMsg="$MSC_LOG_PREFIX"
+  else
+    logLevel=$MSC_LOG_LEVEL
+  fi
 
-    ## Detemine log level
-    if [ -n "${1:-}" ]; then
-        logLevel=$1
-        shift
-    else
-        logLevel=$MSC_LOG_LEVEL
-    fi
+  ## Omit the lower priority
+  if [ $(msc_array_get_value MSC_LOG_LEVEL_DICT $logLevel) -lt $(msc_array_get_value MSC_LOG_LEVEL_DICT $MSC_LOG_LEVEL) ]; then
+    return
+  fi
 
-    ## Omit the lower priority
-    if [ $(msc_array_get_value MSC_LOG_LEVEL_DICT $logLevel) -lt $(msc_array_get_value MSC_LOG_LEVEL_DICT $MSC_LOG_LEVEL) ]; then
-        return
-    fi
-
-    case $logLevel in
+  case $logLevel in
     emerg)
-        finalMsg+="[EMERG] $msg"
-        ;;
+      finalMsg+="[EMERG] $msg"
+      ;;
     alert)
-        finalMsg+="[ALERT] $msg"
-        ;;
+      finalMsg+="[ALERT] $msg"
+      ;;
     crit)
-        finalMsg+="[CRIT] $msg"
-        ;;
+      finalMsg+="[CRIT] $msg"
+      ;;
     err)
-        finalMsg+="[ERR] $msg"
-        ;;
+      finalMsg+="[ERR] $msg"
+      ;;
     warning)
-        finalMsg+="[Warning] $msg"
-        ;;
+      finalMsg+="[Warning] $msg"
+      ;;
     notice)
-        finalMsg+="[notice] $msg"
-        ;;
+      finalMsg+="[notice] $msg"
+      ;;
     *)
-        finalMsg+="$msg"
-        ;;
-    esac
-    logger $MSC_LOG_OPTIONS ${MSC_LOG_TAG:+-t $MSC_LOG_TAG} -p "$MSC_LOG_FACILITY.$logLevel" "$@" "$finalMsg"
+      finalMsg+="$msg"
+      ;;
+  esac
+  logger $MSC_LOG_OPTIONS ${MSC_LOG_TAG:+-t $MSC_LOG_TAG} -p "$MSC_LOG_FACILITY.$logLevel" "$@" "$finalMsg"
 }
 
 ###
 ### msc_exit_status_to_log_level <exitStatus>
 msc_exit_status_to_log_level() {
-    local exitStatus=$1
-    local logLevel
-    if [ $exitStatus -eq $MSC_EXIT_OK ]; then
-        echo info
-    elif [ $exitStatus -le $MSC_EXIT_RETURN ]; then
-        echo $MSC_LOG_LEVEL
-    elif [ $exitStatus -le $MSC_EXIT_WARNING ]; then
-        echo warning
-    elif [ $exitStatus -le $MSC_EXIT_ERR ]; then
-        echo err
-    elif [ $exitStatus -le $MSC_EXIT_CRIT ]; then
-        echo crit
-    else
-        echo alert
-    fi
+  local exitStatus=$1
+  local logLevel
+  if [ $exitStatus -eq $MSC_EXIT_OK ]; then
+    echo info
+  elif [ $exitStatus -le $MSC_EXIT_RETURN ]; then
+    echo $MSC_LOG_LEVEL
+  elif [ $exitStatus -le $MSC_EXIT_WARNING ]; then
+    echo warning
+  elif [ $exitStatus -le $MSC_EXIT_ERR ]; then
+    echo err
+  elif [ $exitStatus -le $MSC_EXIT_CRIT ]; then
+    echo crit
+  else
+    echo alert
+  fi
 }
 
 ###
@@ -124,41 +124,41 @@ msc_exit_status_to_log_level() {
 ###   print error when program exit
 ###   Insert "trap msc_exit_print_error EXIT" to activate the function
 msc_exit_print_error() {
-    local exitStatus=${?:-$MscLogWorstStatusCode}
-    local combinedMsg
-    if [ -n "${MscExitMessageDict[$exitStatus]}" ]; then
-        combinedMsg="${MscExitMessageDict[$exitStatus]}"
-        if [ "x$MscExitMsg" = "x" ]; then
-            msc_log "$combinedMsg." $(msc_exit_status_to_log_level $exitStatus)
-        else
-            msc_log "$combinedMsg: $MscExitMsg" $(msc_exit_status_to_log_level $exitStatus)
-        fi
+  local exitStatus=${?:-$MscLogWorstStatusCode}
+  local combinedMsg
+  if [ -n "${MscExitMessageDict[$exitStatus]}" ]; then
+    combinedMsg="${MscExitMessageDict[$exitStatus]}"
+    if [ "x$MscExitMsg" = "x" ]; then
+      msc_log "$combinedMsg." $(msc_exit_status_to_log_level $exitStatus)
     else
-        msc_log "Undefined Exit Code $exitStatus Error: $MscExitMsg" alert
+      msc_log "$combinedMsg: $MscExitMsg" $(msc_exit_status_to_log_level $exitStatus)
     fi
-    exit $exitStatus
+  else
+    msc_log "Undefined Exit Code $exitStatus Error: $MscExitMsg" alert
+  fi
+  exit $exitStatus
 }
 
 ###
 ### msc_fail <msg> <exitStatus>
-###     Indicate something is wrong.
-###     Exit program if logLevel is equal or larger than MSC_LOG_LEVEL_DICT['crit']
+###   Indicate something is wrong.
+###   Exit program if logLevel is equal or larger than MSC_LOG_LEVEL_DICT['crit']
 msc_fail() {
-    local msg="$1"
-    local exitStatus=$2
-    if [ $exitStatus -ge $MscLogWorstStatusCode ]; then
-        MscLogWorstStatusCode=$exitStatus
-    fi
-    if [[ $exitStatus -gt ${MSC_LOG_LEVEL_DICT['err']} ]]; then
-        ## Crit
-        MscExitMsg="$1"
-        ## Should be handle by msc_exit_print_error
-        exit $exitStatus
-    else
-        ## Not yet exit
-        msc_log "$msg" $(msc_exit_status_to_log_level $exitStatus)
+  local msg="$1"
+  local exitStatus=$2
+  if [ $exitStatus -ge $MscLogWorstStatusCode ]; then
+    MscLogWorstStatusCode=$exitStatus
+  fi
+  if [[ $exitStatus -gt ${MSC_LOG_LEVEL_DICT['err']} ]]; then
+    ## Crit
+    MscExitMsg="$1"
+    ## Should be handle by msc_exit_print_error
+    exit $exitStatus
+  else
+    ## Not yet exit
+    msc_log "$msg" $(msc_exit_status_to_log_level $exitStatus)
 
-    fi
+  fi
 }
 
 ###
@@ -166,95 +166,173 @@ msc_fail() {
 ###    Prints lines whose first non-whitespace character is not '#'
 ###    In other words, this function skip empty, whitespace, or commented line
 msc_file_list_effective() {
-    sed -ne '/^[[:space:]]*[^#]/p' "$1"
+  sed -ne '/^[[:space:]]*[^#]/p'
 }
 
 ###
 ### msc_apply <cmd> <file>
-###   Apply a command to a key=value file.
-###   For example, "msc_apply alias a_file" means
-###   applying alias to each key=value lines in a_file.
+###     Apply a command to a key=value file.
+###     For example, "msc_apply alias a_file" means
+###     applying alias to each key=value lines in a_file.
 msc_apply() {
-    local cmd=$1
-    local file=$2
-    ## Remove the lines with leading '#' and empty lines
-    ## Then eval the: <cmd> <key1>=<value1>; <cmd> <key2>=<value2>...
-    while IFS= read -r line; do
-        eval "$cmd $line"
-    done < <( msc_file_list_effective "$file")
+  local cmd=$1
+  local file="${2:=/dev/stdin}"
+  ## Remove the lines with leading '#' and empty lines
+  ## Then eval the: <cmd> <key1>=<value1>; <cmd> <key2>=<value2>...
+  while IFS= read -r line; do
+    eval "$cmd $line"
+  done < <( msc_file_list_effective "$file")
 }
 
 ###
-### msc_extract_section [-c contentPattern] [-e secEndRule] <secStartPattern>
-###     Extract sections from stdin. If the secStartPattern occurs multiple
-###     times, the contents are appended.
+### msc_count_min_leading_space <file
+###   Return the minmum leading space
+msc_count_min_leading_space() {
+  local file="${1:=/dev/stdin}"
+  local min=-1
+  local curr
+  while IFS= read -r line; do
+    curr=$(awk -F'[^ ]' '{print length($1)}'<<<"$line")
+    if [[ $min -ge $curr ]]; then
+      min=$curr
+    elif [[  $min -eq -1 ]]; then
+      min=$curr
+    fi
+  done < "$file"
+  echo "$min"
+}
+
 ###
-###     Argument:
-###        secStartPattern:
-###           An awk regex pattern without '//' that marks the start of section
-###           e.g. Head[0-9]*
+### msc_extract_section [-s] [-c contentPattern] [-e secEndPattern] <secStartPattern>
+###   Extract sections from stdin. If the secStartPattern occurs multiple times, 
+###   the contents are appended.
 ###
-###     Options
-###        -c contentRule:
-###           An awk pattern that mark the content of section
-###           Default: /^[[:blank:]]+[[:graph:]]/
-###        -e secEndRule:
-###           An awk pattern (rule) that mark the end of section
-###           Default: !($contentRule) && !/$secStartPattern/
+###   Argument:
+###     secStartPattern:
+###       An awk regex pattern without '//' that marks the start of section
+###       e.g. Head[0-9]*
 ###
-###     A file may contains following texts:
+###   Options
+###     -1:
+###       Return only the 1st match
+###     -c <contentPattern>::
+###       An awk pattern that mark the content of section
+###       Default: ^[[:blank:]]+[[:graph:]]
+###     -e <secEndPattern>:
+###       An awk pattern (rule) that mark the end of section
+###       Default: if not specified, string match neither secStartPattern nor contentPattern
 ###
-###         Head1
-###            some text
-###         Head2
-###            some other text
-###         Head3
-###         ...
+###   A file may contains following texts:
 ###
-###      To parse Head2, use following
-###         msc_extract_section Head2 Head3< file
-msc_extract_section() {
-    local OPTIND
-    local secStartPattern
-    local contentRule='/^[[:blank:]]+[[:graph:]]/'
-    local secEndRule
-    while getopts "c:e:" opt; do
-        case $opt in
-        c)
-            contentRule="$OPTARG"
+###     Head1
+###       some text
+###     Head2
+###       some other text
+###     Head3
+###       ...
+###
+###     To parse Head2, use following
+###       msc_extract_section -e Head3 Head2 < file
+msc_extract_section() (
+  local OPTIND
+  local secStartPattern
+  local contentPattern='^[[:blank:]]+[[:graph:]]'
+  local secEndPattern
+
+  local onlyFirst=0
+  local prevState=0  # 0: None, 1: Start, 2: Content, 3: End
+  local newState=0
+  local minSpace=-1
+
+  line_get_new_state(){
+    if egrep "$secStartPattern" <<<"$line" >& /dev/null ; then
+      newState=1
+    elif egrep "$contentPattern" <<<"$line" >& /dev/null ; then
+      newState=2
+    elif egrep "${secEndPattern:-}" <<<"$line" >& /dev/null ; then
+      newState=3
+    else
+      newState=0
+    fi
+  }
+
+  while getopts "1c:e:" opt; do
+    case $opt in
+      1)
+        onlyFirst=1
+        ;;
+      c)
+        contentPattern="$OPTARG"
+        ;;
+      e)
+        secEndPattern="$OPTARG"
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+  secStartPattern="$1"
+
+  while IFS= read -r line; do
+    line_get_new_state
+    ### Whether to change state
+    case $newState in
+      0 )
+        if [[ $prevState -eq 1 || $prevState -eq 2 ]]; then
+          ### No match treated as End
+          newState=3
+          if [[ $onlyFirst  -eq 1 ]]; then
+            return
+          fi
+        fi
+        ;;
+
+      1 )
+        case $prevState in
+          0 | 3)
+            echo "$line"
             ;;
-        e)
-            secEndRule="$OTARG"
+          * )
+            ## Duplicate header, skip
             ;;
         esac
-    done
-    shift $((OPTIND - 1))
-    secStartPattern="$1"
-    if [[ -z "${secEndRules:-}" ]]; then
-        secEndRule='(/^[[:graph:]]/ && !/'"$secStartPattern"'/)'
-    fi
-    awk -v start=0 \
-        '/'"$secStartPattern"'/ {start=1}\
-        start==1 && ('"$secEndRule"') {start=0}\
-        start==1 {print}'
-}
+        ;;
+
+      2 )
+        if [[ $prevState -eq 1 || $prevState -eq 2 ]]; then
+          # Only print content if prev line is either header or content
+          echo "$line"
+        else
+          # Keep the prevState if prev line is neither header nor content
+          newState=$prevState
+        fi
+        ;;
+
+      3 )
+          if [[ $onlyFirst  -eq 1 ]]; then
+            return
+          fi
+        ;;
+    esac
+    prevState=$newState  
+  done
+)
 
 ###
 ### msc_help_print <cmd>
-###     Print in-line help of a program
+###   Print in-line help of a program
 msc_help_print() {
-    local cmd="$1"
-    msc_help_print_simple "$cmd"
-    msc_help_print_simple $MSC_LIBEXEC_DIR/const.sh | msc_extract_section "Exit Status"
+  local cmd="$1"
+  msc_help_print_simple "$cmd"
+  msc_help_print_simple $MSC_LIBEXEC_DIR/const.sh | msc_extract_section "Exit Status"
 }
 
 ###
 ### msc_help_print_simple <cmd>
-###     Print the in-line help of a program itself
-###     It does not print my-sys-cfg Exit Status
+###   Print the in-line help of a program itself
+###   It does not print my-sys-cfg Exit Status
 msc_help_print_simple() {
-    local cmd="$1"
-    sed -rne '/^[[:space:]]*###/ s/^[[:space:]]*###( )?//p' "$cmd"
+  local cmd="$1"
+  sed -rne '/^[[:space:]]*###/ s/^[[:space:]]*###( )?//p' "$cmd"
 }
 
 
@@ -263,52 +341,52 @@ msc_help_print_simple() {
 ###     Whether the string contains substring.
 ###     Returns: 0 if <str> contains <substr>; 1 otherwise
 msc_str_contains_substr() {
-    local str="$1"
-    local substr="$2"
-    if test "${str#*$substr}" != "$str"; then
-        return 0
-    fi
-    return 1
+  local str="$1"
+  local substr="$2"
+  if test "${str#*$substr}" != "$str"; then
+    return 0
+  fi
+  return 1
 }
 
 ### msc_is_interactive_mode
 ###     Whether the shell is in interactive mode.
 ###     Returns: 0 if in interactive mode; 1 otherwise
 msc_is_interactive_mode() {
-    msc_str_contains_substr "$-" i
-    return $?
+  msc_str_contains_substr "$-" i
+  return $?
 }
 
 ## Borrow from /etc/profile
 msc_pathmunge() {
-    case ":${PATH}:" in
+  case ":${PATH}:" in
     *:"$1":*) ;;
 
     *)
-        if [ "$2" = "after" ]; then
-            PATH=$PATH:$1
-        else
-            PATH=$1:$PATH
-        fi
-        ;;
-    esac
+      if [ "$2" = "after" ]; then
+        PATH=$PATH:$1
+      else
+        PATH=$1:$PATH
+      fi
+      ;;
+  esac
 }
 
 ###
 ### msc_quotes_strip
 msc_quotes_strip() {
-    local str="$1"
-    str=${str#[\"]}
-    echo "${str%[\"]}"
+  local str="$1"
+  str=${str#[\"]}
+  echo "${str%[\"]}"
 }
 
 ###
 ### msc_run_and_log <logLevel> <command> [args..]
 msc_run_and_log() {
-    local logLevel=$1
-    shift
-    msc_log "Run: $*" $logLevel
-    $@
+  local logLevel=$1
+  shift
+  msc_log "Run: $*" $logLevel
+  $@
 }
 
 ###
@@ -326,49 +404,49 @@ msc_run_and_log() {
 ###       Use this if process name is different with command
 ###       For example, google-chrome's process name is chrome
 msc_run_if_not_already() {
-    local processName
-    local cmd
-    local baseCmd
-    local sshCmd=""
-    local sshDebugMsg=""
-    local pgrepOptArray=()
-    local opt
-    local OPTIND
+  local processName
+  local cmd
+  local baseCmd
+  local sshCmd=""
+  local sshDebugMsg=""
+  local pgrepOptArray=()
+  local opt
+  local OPTIND
 
-    while getopts "a:c:n:u:" opt; do
-        case $opt in
-        a)
-            sshCmd="ssh $OPTARG"
-            sshDebugMsg=" with SSH arg $OPTARG"
-            ;;
-        c)
-            cmd="$OPTARG"
-            ;;
-        n)
-            processName="${2:0:15}"
-            ;;
-        u)
-            user="$OPTARG"
-            pgrepOptArray+=(-u "$2")
-            ;;
-        esac
-    done
-    shift $((OPTIND - 1))
+  while getopts "a:c:n:u:" opt; do
+    case $opt in
+      a)
+        sshCmd="ssh $OPTARG"
+        sshDebugMsg=" with SSH arg $OPTARG"
+        ;;
+      c)
+        cmd="$OPTARG"
+        ;;
+      n)
+        processName="${2:0:15}"
+        ;;
+      u)
+        user="$OPTARG"
+        pgrepOptArray+=(-u "$2")
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
 
-    : ${cmd:=$1}
-    baseCmd=$(basename $cmd)
+  : ${cmd:=$1}
+  baseCmd=$(basename $cmd)
 
-    ## pgrep truncate the baseCmd to first 15 characters
-    : ${processName:=${baseCmd:0:15}}
+  ## pgrep truncate the baseCmd to first 15 characters
+  : ${processName:=${baseCmd:0:15}}
 
-    if eval "${sshCmd} pgrep ${pgrepOptArray[@]} -lx $processName" &>/dev/null; then
-        msc_log "already run $processName$sshDebugMsg" notice
+  if eval "${sshCmd} pgrep ${pgrepOptArray[@]} -lx $processName" &>/dev/null; then
+    msc_log "already run $processName$sshDebugMsg" notice
+  else
+    if eval "${sshCmd} which $cmd" >&/dev/null; then
+      msc_log "run_if_not_already run $processName: ${sshCmd[*]} $*" info
+      eval "$sshCmd $@" &
     else
-        if eval "${sshCmd} which $cmd" >&/dev/null; then
-            msc_log "run_if_not_already run $processName: ${sshCmd[*]} $*" info
-            eval "$sshCmd $@" &
-        else
-            msc_log "run_if_not_already: skip the missing command $cmd$sshDebugMsg" warning
-        fi
+      msc_log "run_if_not_already: skip the missing command $cmd$sshDebugMsg" warning
     fi
+  fi
 }
